8056b2338f72e15026f17ca050dd547ee162f30f
jason <zzhou292@wisc.edu>
2023-10-23 01:04:01 -0500

    fixed usage

--- tracetools_analysis/tracetools_analysis/utils/ros2.py
+++ tracetools_analysis/tracetools_analysis/utils/ros2.py
@@ -49,62 +49,50 @@ class Ros2DataModelUtil(DataModelUtil):
     def data(self) -> Ros2DataModel:
         return super().data  # type: ignore
 
-    def _prettify(
-        self,
-        original: str,
-    ) -> str:
-        """
-        Process symbol to make it more readable.
-
-        * remove std::allocator
-        * remove std::default_delete
-        * bind object: remove placeholder
-
-        :param original: the original symbol
-        :return: the prettified symbol
-        """
+    def _prettify(self, original: str) -> str:
         pretty = original
-        # remove spaces
-        pretty = pretty.replace(' ', '')
-        # allocator
-        std_allocator = '_<std::allocator<void>>'
-        pretty = pretty.replace(std_allocator, '')
-        # default_delete
-        std_defaultdelete = 'std::default_delete'
-        if std_defaultdelete in pretty:
-            dd_start = pretty.find(std_defaultdelete)
-            template_param_open = dd_start + len(std_defaultdelete)
-            # find index of matching/closing GT sign
-            template_param_close = template_param_open
-            level = 0
-            done = False
-            while not done:
-                template_param_close += 1
-                if pretty[template_param_close] == '<':
-                    level += 1
-                elif pretty[template_param_close] == '>':
-                    if level == 0:
-                        done = True
-                    else:
-                        level -= 1
-            pretty = pretty[:dd_start] + pretty[(template_param_close + 1):]
-        # bind
-        std_bind = 'std::_Bind<'
-        if pretty.startswith(std_bind):
-            # remove bind<>
-            pretty = pretty.replace(std_bind, '')
-            pretty = pretty[:-1]
-            # remove placeholder stuff
-            placeholder_from = pretty.find('*')
-            placeholder_to = pretty.find(')', placeholder_from)
-            pretty = pretty[:placeholder_from] + '?' + pretty[(placeholder_to + 1):]
-        # remove dangling comma
-        pretty = pretty.replace(',>', '>')
-        # restore meaningful spaces
-        if pretty.startswith('void'):
-            pretty = 'void' + ' ' + pretty[len('void'):]
-        if pretty.endswith('const'):
-            pretty = pretty[:(len(pretty) - len('const'))] + ' ' + 'const'
+        if isinstance(pretty, str):  # Check if pretty is a string
+            # remove spaces
+            pretty = pretty.replace(' ', '')
+            # allocator
+            std_allocator = '_<std::allocator<void>>'
+            pretty = pretty.replace(std_allocator, '')
+            # default_delete
+            std_defaultdelete = 'std::default_delete'
+            if std_defaultdelete in pretty:
+                dd_start = pretty.find(std_defaultdelete)
+                template_param_open = dd_start + len(std_defaultdelete)
+                # find index of matching/closing GT sign
+                template_param_close = template_param_open
+                level = 0
+                done = False
+                while not done:
+                    template_param_close += 1
+                    if pretty[template_param_close] == '<':
+                        level += 1
+                    elif pretty[template_param_close] == '>':
+                        if level == 0:
+                            done = True
+                        else:
+                            level -= 1
+                pretty = pretty[:dd_start] + pretty[(template_param_close + 1):]
+            # bind
+            std_bind = 'std::_Bind<'
+            if pretty.startswith(std_bind):
+                # remove bind<>
+                pretty = pretty.replace(std_bind, '')
+                pretty = pretty[:-1]
+                # remove placeholder stuff
+                placeholder_from = pretty.find('*')
+                placeholder_to = pretty.find(')', placeholder_from)
+                pretty = pretty[:placeholder_from] + '?' + pretty[(placeholder_to + 1):]
+            # remove dangling comma
+            pretty = pretty.replace(',>', '>')
+            # restore meaningful spaces
+            if pretty.startswith('void'):
+                pretty = 'void' + ' ' + pretty[len('void'):]
+            if pretty.endswith('const'):
+                pretty = pretty[:(len(pretty) - len('const'))] + ' ' + 'const'
         return pretty
 
     def get_callback_symbols(self) -> Mapping[int, str]:
@@ -455,12 +443,13 @@ class Ros2DataModelUtil(DataModelUtil):
         :param node_handle: the node handle value
         :return: a dictionary with name:value info, or `None` if it fails
         """
+        node_handle = str(node_handle)  # Convert to a string
         if node_handle not in self.data.nodes.index:
             return None
-
         node_name = self.data.nodes.loc[node_handle, 'name']
         tid = self.data.nodes.loc[node_handle, 'tid']
         return {'node': node_name, 'tid': tid}
+
 
     def get_lifecycle_node_handle_info(
         self,
